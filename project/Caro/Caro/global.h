f781b62a4f4d118f8ebba2c99a5c381096fce475
HwngMP <pmai4118@gmail.com>
2025-11-16 21:35:20 +0700

    tao foder setting

--- project/Caro/Caro/global.h
+++ project/Caro/Caro/global.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "button/button.h"
 #include "keyboard/keyboard.h"
 #include "boardgame/boardgame.h"
 #include "menu/menu.h"
 #include "startgame/startgame.h"
 #include "loadingscreen/loadingscreen.h"
+#include "sfx/sfx.h"
 #include <vector>
 #include <iostream>
 #include <string>
